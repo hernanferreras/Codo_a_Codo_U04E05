dbbafa414c8a0b421c1a8b2827d9e7700cd3a3a1
maxher73 <maxher73@yahoo.com.ar>
2020-03-23 20:23:54 -0300

    Unidad 4 Ejercicio 5 de la practica de Codo a Codo

--- src/u04e05/U04E05.java
+++ src/u04e05/U04E05.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package u04e05;
 
-/**
- *
- * @author hernan
- */
+import java.util.Scanner;
 public class U04E05 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String args[]) {
+      Scanner entrada = new Scanner (System.in);
+      int radio;
+      double perimetro, superficie;
+      final double pi = Math.PI;
+      System.out.print("Ingrese el radio del circulo: ");
+      radio = entrada.nextInt();
+      superficie = pi*(Math.pow(radio,2));
+      perimetro = (2*pi*radio);
+      System.out.println("La superficie del circulo es igual a; "+superficie);
+      System.out.print("El perimetro del circulo es igual a; "+perimetro);
     }
-    
 }
